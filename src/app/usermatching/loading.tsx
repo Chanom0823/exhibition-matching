d66f39347cd6b466519a39f6d845407ae10d15cc
Tanagon492547 <155215128+Tanagon492547@users.noreply.github.com>
2025-12-10 21:41:09 +0700

    edit: เพิ่มหน้าโหลด UI สำหรับรอ usermatching

---
+++ src/app/usermatching/loading.tsx
@@ -0,0 +1,11 @@
+const Loading =()=> {
+  // Or a custom loading skeleton component
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <p>Loading...</p>
+    </div>
+  )
+}
+
+
+export default Loading;
